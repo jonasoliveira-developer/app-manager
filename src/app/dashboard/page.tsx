643900b89e9b636195e772fe4d5afe8d5323a1ec
jonasoliveira-developer <jonasoliveira.webdeveloper@gmail.com>
2025-09-06 02:37:52 -0300

    exlui lib não usada

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 import { Container } from "@/components/container"
 import { CareCalendar } from "@/app/dashboard/components/calendar"
-import PrivateRoute from "@/components/private"
 
 export default function Dashboard() {
     return (
